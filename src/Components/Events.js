074224523430380ca52a1a300aa4442497208a37
Aryan Gadhiya <ary16.work@gmail.com>
2023-09-25 11:55:12 +0530

    Events Updated

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './events.css'
+import eventsjson from './events.json'
 
 function Events() {
   return (
     <div className='events'>
       <p className="eventstitle">IEEE PELS VIT EVENTS</p>
       <ul>
-        <li>Event-1</li>
-        <li>Event-2</li>
-        <li>Event-3</li>
-        <li>Event-4</li>
-        <li>Event-5</li>
+        {
+          eventsjson
+          && eventsjson.map(eventsjson => <a href={eventsjson.event_link} rel='noreferrer' target="_blank"><li>{eventsjson.event_title}</li></a>)
+        }
       </ul>
     </div>
   )
