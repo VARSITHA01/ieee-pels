9041034020f17e70a9012abcad90bd7b551eee8a
Aryan Gadhiya <ary16.work@gmail.com>
2023-10-11 20:56:55 +0530

    Updated

--- src/Components/Headertop.js
+++ src/Components/Headertop.js
@@ -6,9 +6,9 @@ function Headertop() {
   return (
     <div className='Headertop'>
       <Link to={"https://www.ieee.org"} rel='noreferrer' target="_blank"><li>IEEE.org</li></Link>
-      <Link to={"https://ieeexplore.ieee.org/Xplore/home.jsp"} rel='noreferrer' target="_blank"><li>IEEE Xplore Digital Library</li></Link>
-      <Link to={"http://standards.ieee.org"} rel='noreferrer' target="_blank"><li>IEEE Standards</li></Link>
-      <Link to={"http://spectrum.ieee.org"} rel='noreferrer' target="_blank"><li>IEEE Spectrum</li></Link>
+      <Link to={"https://ieeexplore.ieee.org/Xplore/home.jsp"} rel='noreferrer' target="_blank"><li className='hidden_top_header'>IEEE Xplore Digital Library</li></Link>
+      <Link to={"http://standards.ieee.org"} rel='noreferrer' target="_blank"><li className='hidden_top_header'>IEEE Standards</li></Link>
+      <Link to={"http://spectrum.ieee.org"} rel='noreferrer' target="_blank"><li className='hidden_top_header'>IEEE Spectrum</li></Link>
       <Link to={"https://www.ieee.org/sitemap.html"} rel='noreferrer' target="_blank"><li>More Sites</li></Link>
     </div>
   )
