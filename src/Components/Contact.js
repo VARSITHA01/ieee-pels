ae0bd66b1b6a4268f364c68e3340adf31139d310
Aryan Gadhiya <ary16.work@gmail.com>
2023-10-11 13:41:42 +0530

    Minor Updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,6 +5,16 @@ function Contact() {
   return (
     <div className='contact'>
       <p className="contacttitle">Contact Details</p>
+      <div className="contact_details">
+        <div className="contact_name">Dr. Albert Alexander S.</div>
+        <div className="contact_position">Faculty Co-ordinator</div>
+        <div className="contact_email">ootyalex@gmail.com</div>
+      </div>
+      <div className="contact_details">
+        <div className="contact_name">Dr. Chitra A.</div>
+        <div className="contact_position">Faculty Co-ordinator</div>
+        <div className="contact_email">chitra.a@vit.ac.in</div>
+      </div>
     </div>
   )
 }
