53218c74171d9327bb4e47dbfd5a905433dc7a06
Harsh Raj <128452067+harshraj2008@users.noreply.github.com>
2024-12-24 09:02:01 +0530

    advisory board added

--- src/Components/Committee.js
+++ src/Components/Committee.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './committee.css'
 import members from './committee_members.json'
+import advisoryBoard from './advisoryboard.json'
 import MemberCards from './MemberCards'
 
 function Committee() {
@@ -9,6 +10,7 @@ function Committee() {
 
   // Filter members based on the selected tenure
   const filteredMembers = members.filter(member => member.tenure === selectedTenure);
+  const filteredAdvisoryBoard = advisoryBoard.filter(member => member.tenure === selectedTenure);
 
   // Handle tenure selection
   const handleTenureSelection = (tenure) => {
@@ -27,10 +29,10 @@ function Committee() {
         <div className="dropdown-content">
           <a href="#" onClick={() => handleTenureSelection('2023-2024')}>2023-2024</a>
           <a href="#" onClick={() => handleTenureSelection('2024-2025')}>2024-2025</a>
-          <a href="#" onClick={() => handleTenureSelection('2025-2026')}>2025-2026</a>
         </div>
       </div>
 
+      {/* Committee Members Section */}
       <div className="committeemembers">
         <div className="container membersx">
           <div className="row">
@@ -48,8 +50,32 @@ function Committee() {
           </div>
         </div>
       </div>
+
+      {/* Advisory Board Section if tenure is 2024-2025 */}
+      {selectedTenure === '2024-2025' && (
+        <div className="advisoryboard">
+          <center>
+            <div className="advisorytitle">Advisory Board</div>
+          </center>
+          <div className="container membersx">
+            <div className="row">
+              {
+                filteredAdvisoryBoard.map(member => (
+                  <div className="col-sm-4 colx" key={member.id}>
+                    <MemberCards
+                      member_img_link={member.member_img_link}
+                      member_name={member.member_name}
+                      member_position={member.member_position}
+                    />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
 
-export default Committee
+export default Committee;
